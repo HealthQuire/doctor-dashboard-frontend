3feda9db0390e43fa3b770358edb67e2b9347d5f
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-03-11 06:45:38 +0300

    I hate my life. All done except login

--- src/pages/timetable-page/styles.ts
+++ src/pages/timetable-page/styles.ts
@@ -1,16 +1,109 @@
 import styled from 'styled-components';
+import theme from '../../styles/theme.ts';
 
 export const TimeTableWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 20px;
+    width: 100%;
 `;
 
-export const TimeTableTitle = styled.div``;
+export const TimeTableTitle = styled.div`
+    font-size: 36px;
+    font-weight: 900;
+    margin: 16px;
+`;
 
-export const TimeTableContainer = styled.div``;
+export const TimeTableContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: #ffffff;
+    color: #999999;
+    font-weight: bolder;
+    padding: 20px;
+`;
 
-export const TimeTableDayColumn = styled.div``;
+export const TimeTableDayColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 
-export const TimeTableDayCell = styled.div``;
+export const TimeTableDayColumnTitle = styled.div``;
+
+export const TimeTableDayCell = styled.div`
+    display: flex;
+    justify-content: end;
+    align-items: end;
+    width: 150px;
+    height: 60px;
+    border: 1px solid #eeeeee;
+    font-size: 20px;
+`;
+
+export const ActiveTimeTableDayCell = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+    flex-direction: column;
+    width: 150px;
+    height: 60px;
+    color: ${theme.colors.accentOne};
+    border: 1px solid #eeeeee;
+    font-size: 20px;
+    cursor: pointer;
+`;
+
+export const PopupContainer = styled.div`
+    position: fixed;
+    z-index: 200;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const PopupWrapper = styled.div`
+    width: 400px;
+    min-height: 20vh;
+    background-color: ${theme.colors.bgPrimary};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 64px;
+    padding: 64px;
+`;
+
+export const PopupTitle = styled.div`
+    font-size: 24px;
+    font-weight: bold;
+`;
+
+export const PopupContent = styled.text`
+    text-align: left;
+    background-color: #ffffff;
+    color: #000000;
+    margin: 32px 0;
+    padding: 32px;
+`;
+
+export const PopupButton = styled.div`
+    width: 200px;
+    color: #ffffff;
+    text-align: center;
+    text-decoration: none;
+    background-color: ${theme.colors.accentOne};
+    padding: 14px 20px;
+    border: none;
+    cursor: pointer;
+    transition: 0.3s;
+    border-radius: 8px;
+    &:hover {
+        background-color: ${theme.colors.accentTwo};
+    }
+`;
--- src/pages/timetable-page/timetable-page.tsx
+++ src/pages/timetable-page/timetable-page.tsx
@@ -1,5 +1,20 @@
-import { TimeTableWrapper } from './styles.ts';
-import { useState } from 'react';
+import {
+    ActiveTimeTableDayCell,
+    PopupButton,
+    PopupContainer,
+    PopupTitle,
+    PopupWrapper,
+    TimeTableContainer,
+    TimeTableDayCell,
+    TimeTableDayColumn,
+    TimeTableDayColumnTitle,
+    TimeTableTitle,
+    TimeTableWrapper
+} from './styles.ts';
+import { useEffect, useState } from 'react';
+import { useGetCurrentWeekDoctorTimeCellsQuery } from '../../store/api/timecell-api.ts';
+import TimetablePage from './timetable-page.tsx';
+import { customerApi, ICustomer } from '../../store/api/customer-api.ts';
 
 const dayTimes = [
     '7:00',
@@ -17,10 +32,81 @@ const dayTimes = [
     '19:00'
 ];
 
+const getWeekDay = (index: number) => {
+    const date = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return String(date.getDate() + index) + ' ' + days[(date.getDay() + index) % 7];
+};
+
 const TimeTable = () => {
     const [popupActive, setPopupActive] = useState(false);
-    const currentWeekCells = useGetCurre;
-    return <TimeTableWrapper>time table wrapper</TimeTableWrapper>;
+    const [timeTableCanvas, setTimeTableCanvas] = useState<Array<Array<string>>>([]);
+    const currentWeekCells = useGetCurrentWeekDoctorTimeCellsQuery();
+    const [currentCustomer, setCurrentCustomer] = useState<ICustomer | null>(null);
+
+    useEffect(() => {
+        setTimeTableCanvas(new Array(7).fill(null).map(() => dayTimes));
+    }, []);
+
+    useEffect(() => {
+        console.log(currentWeekCells.data);
+    }, [currentWeekCells]);
+
+    const handlePopup = (customer: ICustomer) => {
+        setCurrentCustomer(customer);
+        setPopupActive(true);
+    };
+
+    return (
+        <TimeTableWrapper>
+            <TimeTableTitle>Timetable</TimeTableTitle>
+            <TimeTableContainer>
+                {timeTableCanvas.map((day, dayIndex) => (
+                    <TimeTableDayColumn key={dayIndex}>
+                        <TimeTableDayColumnTitle> {getWeekDay(dayIndex)} </TimeTableDayColumnTitle>
+                        {day.map((time, timeIndex) => {
+                            const today = new Date();
+                            today.setDate(today.getDate() + dayIndex);
+                            today.setHours(3);
+                            today.setMinutes(0);
+                            today.setSeconds(0);
+                            today.setMilliseconds(0);
+                            //console.log(today.toUTCString());
+                            const active = currentWeekCells.data?.find((item) => {
+                                return (
+                                    item.time == time &&
+                                    String(new Date(item.date)) == String(today)
+                                );
+                            });
+                            return active ? (
+                                <ActiveTimeTableDayCell
+                                    onClick={() => handlePopup(active.customer)}
+                                    key={timeIndex}
+                                >
+                                    <div style={{ fontSize: '16px' }}>
+                                        {active.customer.lastname} {active.customer.firstname}
+                                    </div>
+                                    {time}
+                                </ActiveTimeTableDayCell>
+                            ) : (
+                                <TimeTableDayCell key={timeIndex}>{time}</TimeTableDayCell>
+                            );
+                        })}
+                    </TimeTableDayColumn>
+                ))}
+            </TimeTableContainer>
+            {popupActive && (
+                <PopupContainer>
+                    <PopupWrapper>
+                        <PopupTitle>
+                            {currentCustomer?.lastname} {currentCustomer?.firstname}
+                        </PopupTitle>
+                        <PopupButton onClick={() => setPopupActive(false)}>Continue</PopupButton>
+                    </PopupWrapper>
+                </PopupContainer>
+            )}
+        </TimeTableWrapper>
+    );
 };
 
 export default TimeTable;
